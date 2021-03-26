c3329265828a2dfe4e19b7d5a6fff3d02dc5b4dd
Nishigaki2706 <cn34927nc@gmail.com>
2021-03-26 22:40:55 +0900

    [add]記事詳細画面

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,7 @@
     <body>
         <h1>Blog Name</h1>
         <p class='create'>[<a href='/posts/create'>create</a>]</p>
+         </form>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
@@ -17,6 +18,11 @@
                 </div>
             @endforeach
         </div>
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+            @csrf
+            @method('DELETE')
+         <button type="submit">delete</button> 
+         </form>
         <div class='paginate'>
             {{ $posts->links() }}
         </div>
